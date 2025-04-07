a0d6693d678725fe0b0bb238f4d91b90e1c2162f
Daljeet Singh <singhdaljeet2006@gmail.com>
2025-04-07 15:22:22 +0530

    qq

--- src/Website/main.jsx
+++ src/Website/main.jsx
@@ -7,11 +7,11 @@ const LogoCard = () => {
                 <div className="flex  bg-blue-600 gap-4 items-center justify-between  p-4 rounded-b-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:bg-blue-700 hover:shadow-2xl">
                     {/* Logo + Title */}
                     <div className="flex items-center gap-4">
-                        <img
+                        {/* <img
                             src="37cde907-e8a2-45d4-9a6a-05d5cac3ff6c.jpeg" // <-- Put your image filename here
                             alt="logo"
                             className="h-16 w-16 rounded-2xl"
-                        />
+                        /> */}
                         <h1 className="text-white text-3xl font-bold tracking-wide">
                             Info<span className="text-yellow-300">2</span>Docs
                         </h1>
